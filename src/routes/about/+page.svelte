7f690d8cb8a08d841ad22d610f21e25aea50c907
Zakrok09 <eccyboo@protonmail.com>
2022-08-29 22:45:09 +0200

    Update certain texts and change favicon location

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,17 @@
+<script>
+    let number1;
+    let number2;
+
+    $:num3 = Number.parseInt(number1) + Number.parseInt(number2);
+</script>
+
+<div class="felx">
+    <input type="text" bind:value={number1}>
+    <input type="text" bind:value={number2}>
+    <p>{num3}</p>
+</div>
+
 <p>nothing hre bro</p>
+
+<style lang="scss">
+</style>
--- src/routes/about/us/+page.svelte
+++ src/routes/about/us/+page.svelte
@@ -1,1 +1,1 @@
-<p>nothing hre bro</p>
+<p>About us</p>
