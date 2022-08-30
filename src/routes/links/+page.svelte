97c6647ddb4b99ff3220416e075c044458bcda97
Zakrok09 <eccyboo@protonmail.com>
2022-08-31 00:47:38 +0200

    Setup links page

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,6 +1,43 @@
-
 <script>
     export let data;
+    let categories = data.links.categories;
+
+    import Linkbox from "$lib/Reusable/Linkbox.svelte";
 </script>
 
-<p>{data.links}</p>
+{#each categories as cat }
+    <div class="category">
+        <h2>{cat.name}</h2>
+        
+        <div class="links">
+            {#each cat.links as link }
+                <!-- <a href={link.eng.link}>{link.eng.presName}</a> -->
+                <Linkbox href={link.eng.link} title={link.eng.presName} />
+            {/each}    
+        </div>
+            
+    </div>
+{/each}
+
+<style lang="scss">
+    *  {
+        font-family: 'Oxygen', sans-serif;
+    }
+
+    h2 {
+        padding: 20px 50px;
+        font-size: 2rem;
+    }
+
+    .category {
+        width: 100%;
+        border-top: 1px solid black;
+    }
+
+    .links {
+        margin: 50px;
+        display: flex;
+        flex-flow: row wrap;
+        gap: 30px;
+    }
+</style>
