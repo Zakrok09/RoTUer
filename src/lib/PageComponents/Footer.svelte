b59dbdcb741326af991fdf0bc172af9ff059b07a
Zakrok09 <eccyboo@protonmail.com>
2022-08-30 16:24:16 +0200

    Fix important typo

--- src/lib/PageComponents/Footer.svelte
+++ src/lib/PageComponents/Footer.svelte
@@ -3,7 +3,7 @@
 <footer>
     <!-- ! DO NOT REMOVE THIS --> <p>Free SVG Background by <a target="_blank" href="https://bgjar.com">BGJar</a></p>
     <p>DISCLAIMER: This website is not affiliated with TU Delft in any way. Information here is gathered and presented by us (the team behind the project). </p>
-    <p>Website developed by the Praxidike</p>
+    <p>Website developed by Praxidike</p>
 
     <p>Some photos you might see on this website are not owned by us and are purely for visual appeal (mostly favicons related to their link). If you want us to remove them please inform us and we will do it ASAP.</p>
 </footer>
