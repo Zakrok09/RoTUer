565581d035f26c2520e6d9d39d061a5a5e13bff5
Zakrok09 <eccyboo@protonmail.com>
2022-09-03 11:32:53 +0200

    Make dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import { t, locale } from "$lib/translations/i18n";
     import HEAD from "$lib/Metadata/HEAD.svelte";
+    import Dashboardlink from "$lib/Reusable/Dashboardlink.svelte"
+	import { fly } from 'svelte/transition';
     export let data;
     const db = data.database;
     const programmeTags = db.programmesTags;
@@ -8,10 +10,19 @@
     const programmes = db.programmes;
     
     
-    let activeProgramme = "common";
+    let activeProgrammeString = "common";
+    $: activeProgrammeObject = programmes[activeProgrammeString.toString()];
     let programmeTagsArray = Object.keys(programmeTags);
     let searchString = '';
     $:searchArray = [''];
+    let activeTag = "all";
+
+    function containsTag(tagsOfLink, chosenTag) {
+        if (chosenTag === 'all') return true;
+        let result = false;
+        if (tagsOfLink.some(linkTag => linkTag === chosenTag)) result = true;
+        return result;
+    }
 
     function containsKeyword(linkKeywords, searchBoxKeywords) {
         if (searchBoxKeywords.length === 1 && searchBoxKeywords[0] === "" ) return true;
@@ -24,6 +35,13 @@
         searchArray = searchString.toLocaleLowerCase().split(' ');
     }
 
+    function changeActive(e) {
+        if (activeTag === e.detail) {
+            activeTag = "all"
+        } else {
+            activeTag = e.detail;
+        }
+    }
 </script>
 
 <HEAD title="Dashboard" metadescription="RoTUer - a free, open-source dashboard that students need! RoTUer is a project aimed at helping students by providing them valuable information." />
@@ -36,22 +54,34 @@
         <i>{$t("dashboard.underheading")}</i>
     </div>
     
-    <div class="dashboard">
-        <div class="filters">
-            <select bind:value={activeProgramme}>
-                {#each programmeTagsArray as programmeTag}
-                    <option value={programmeTag}>{programmeTags[programmeTag].pressNames[$locale.toString()]}</option>
-                {/each}
-            </select>
-            <form class="searchForm" on:submit|preventDefault={updateSearchArray}>
-                <input type="text" bind:value={searchString} placeholder="Search...">
-                <button type="submit">🔎</button>
-            </form>
-        </div>
-        <div class="displayPanel">
-            <!-- {#each programmes as }
-                
-            {/each} -->
+    <div class="dashboardBg">
+        <div class="dashboard">
+            <div class="filters">
+                <select class="filterButtons" bind:value={activeProgrammeString}>
+                    {#each programmeTagsArray as programmeTag}
+                        <option value={programmeTag}>{programmeTags[programmeTag].pressNames[$locale.toString()]}</option>
+                    {/each}
+                </select>
+                <form class="filterButtons searchForm" on:submit|preventDefault={updateSearchArray}>
+                    <input type="text" bind:value={searchString} placeholder="Search...">
+                    <button type="submit">🔎</button>
+                </form>
+            </div>
+            <div class="displayPanel">
+                <div class="panelHead">
+                    <h2>{activeProgrammeObject.name}</h2>
+                    <p>Showing links for {activeProgrammeObject.abbr}</p>
+                </div>
+                <div class="links">
+                    {#each activeProgrammeObject.links as link}
+                        {#if containsTag(link.tags, activeTag) && containsKeyword(link.keywords[$locale.toString()], searchArray)}
+                            <div transition:fly="{{ y: 10, duration: 100 }}">
+                                <Dashboardlink on:changeActiveTag={changeActive} desc={link[$locale.toString()].desc} href={link[$locale.toString()].link} title={link.name} tags={link.tags} tagsObject={tags}/>
+                            </div>
+                        {/if}
+                    {/each}
+                </div>
+            </div>
         </div>
     </div>
 </main>
@@ -59,8 +89,81 @@
 <style lang="scss">
     $contrast_header: #24313d;
 
+    .panelHead {
+        h2, p {
+            color: #24313d;
+            text-align: center;
+        }
+        border-bottom: 2px solid hsla(209, 26%, 19%, 0.353);
+    }
+
+    .links {
+        padding: 50px;
+        display: flex;
+        flex-flow: row wrap;
+        gap: 20px;
+        justify-content: center;
+    }
+
+    .dashboardBg {
+        background-image: url("/background/bubbleBackground.svg");
+        width: 100vw;
+        padding: 5vw;
+    }
+
+    .searchForm {
+        display: flex;
+        position: relative;
+        padding: 0px;
+        width: 200px !important;
+
+        input {
+            flex-grow: 1;
+            width: auto;
+            height: 30px;
+            padding: 5px 0px 5px 10px;
+            border: none;
+            border-radius: 15px;
+        }
+    }
+
+    button {
+        background-color: #e8e8e8;
+        cursor: pointer;
+        border: none;
+        font-size: 2rem;
+        border-radius: 15px;
+    }
+
+    .dashboard {
+        width: 90vw;
+        background-color: white;
+        border-radius: 15px;
+    }
+
+    .filters {
+        padding: 20px 25px;
+        display: flex;
+        align-items: center;
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;
+        background-color: #c9c9c9;
+    }
+
     * {
         font-family: 'Oxygen', sans-serif;
+    }
+
+    .filterButtons {
+        border-radius: 15px;
+        border: none;
+        color: rgb(0, 0, 0);
+        width: 170px;
+        font-weight: bold;
+        padding: 5px 10px;
+        font-size: 0.8rem;
+        cursor: pointer;
+        height: 40px;
     }
 
     .landingSite {
@@ -84,4 +187,55 @@
     }
 
     
+
+    @media only screen and (max-width: 550px) {
+        .filters {
+            display: flex;
+            flex-flow: column nowrap;
+            gap: 15px;
+            align-items: center;
+        }
+
+        .filterButtons {
+            width: 300px;
+            padding: 10px 10px;
+            font-size: 0.9rem;
+        }
+
+        .searchForm {
+            width: 300px;
+
+            input {
+                width: 290px;
+            }
+        }
+    }
+
+    @media only screen and (max-height: 600px) {
+        .filters {
+            display: flex;
+            flex-flow: row wrap;
+            gap: 5px;
+            justify-content: center;
+        }
+
+        .filterButtons {
+            width: auto;
+            padding: 10px 10px;
+            font-size: 0.75rem;
+        }
+
+        .searchForm {
+            width: 200px;
+
+            input {
+                font-size: 0.75rem;
+                width: 190px;
+            }
+            button {
+                
+                font-size: 1.3rem;
+            }
+        }
+    }
 </style>
