aa29bbca8d83df9403d196265a59b6fe93adcc84
Zakrok09 <eccyboo@protonmail.com>
2022-09-03 10:54:18 +0200

    Update dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,29 @@
 <script>
-    import { t } from "$lib/translations/i18n";
+    import { t, locale } from "$lib/translations/i18n";
     import HEAD from "$lib/Metadata/HEAD.svelte";
+    export let data;
+    const db = data.database;
+    const programmeTags = db.programmesTags;
+    const tags = db.tags;
+    const programmes = db.programmes;
+    
+    
+    let activeProgramme = "common";
+    let programmeTagsArray = Object.keys(programmeTags);
+    let searchString = '';
+    $:searchArray = [''];
+
+    function containsKeyword(linkKeywords, searchBoxKeywords) {
+        if (searchBoxKeywords.length === 1 && searchBoxKeywords[0] === "" ) return true;
+        let result = false;
+        if (linkKeywords.some(linkKeyword => searchBoxKeywords.includes(linkKeyword))) return true;
+        return result;
+    }
+
+    function updateSearchArray() {
+        searchArray = searchString.toLocaleLowerCase().split(' ');
+    }
+
 </script>
 
 <HEAD title="Dashboard" metadescription="RoTUer - a free, open-source dashboard that students need! RoTUer is a project aimed at helping students by providing them valuable information." />
@@ -13,6 +36,24 @@
         <i>{$t("dashboard.underheading")}</i>
     </div>
     
+    <div class="dashboard">
+        <div class="filters">
+            <select bind:value={activeProgramme}>
+                {#each programmeTagsArray as programmeTag}
+                    <option value={programmeTag}>{programmeTags[programmeTag].pressNames[$locale.toString()]}</option>
+                {/each}
+            </select>
+            <form class="searchForm" on:submit|preventDefault={updateSearchArray}>
+                <input type="text" bind:value={searchString} placeholder="Search...">
+                <button type="submit">🔎</button>
+            </form>
+        </div>
+        <div class="displayPanel">
+            <!-- {#each programmes as }
+                
+            {/each} -->
+        </div>
+    </div>
 </main>
 
 <style lang="scss">
