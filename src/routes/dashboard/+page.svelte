22415860fddc9626f119ce688c8133525d52226a
Zakrok09 <eccyboo@protonmail.com>
2022-09-04 19:16:05 +0200

    Fix design issues in dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -129,14 +129,17 @@
     }
 </script>
 
+<svelte:head>
+    <style>
+        body {
+            background-color: black;
+        }
+    </style>
+</svelte:head>
+
 <HEAD title="Dashboard" metadescription="The dashboard is that cool place where you have quick access to all the links related to your study program! Come to read or contribute!" />
 
 <main>
-    <div class="landingSite">
-        <img src="/isometric/logo.svg" alt="The logo of RoTUer">
-        <h1 tabindex="0">RoTUer</h1>
-        <i>{$t("dashboard.underheading")}</i>
-    </div>
     
     <div class="restOfPage">
         <div class="dashboardBg">
@@ -193,6 +196,11 @@
 <style lang="scss">
     $contrast_header: #24313d;
 
+    .restOfPage {
+        background-image: url("/background/dashboard.svg");
+        background-size: cover;
+    }
+
     .facultyLinks {
         display: flex;
         flex-flow: row wrap;
@@ -222,7 +230,6 @@
     .underDashBg {
         position: relative;
         margin-top: -1px;
-        background-image: url("/background/simpleshiny.svg");
         background-size: cover;
     }
 
@@ -273,7 +280,6 @@
     }
 
     .dashboardBg {
-        background-image: url("/background/bubbleBackground.svg");
         width: 100vw;
         padding-top: 20px;
         display: flex;
@@ -334,26 +340,6 @@
         font-size: 0.8rem;
         cursor: pointer;
         height: 40px;
-    }
-
-    .landingSite {
-        padding-top: 70px;
-        padding-bottom: 200px;
-        font-family: 'Oxygen', sans-serif;
-        text-align: center;
-
-        display: flex;
-        flex-flow: column nowrap;
-        align-items: center;
-
-        i {
-            font-size: 0.9rem;
-        }
-
-        img {
-            width: 300px;
-            display: inline;
-        }
     }
 
     
